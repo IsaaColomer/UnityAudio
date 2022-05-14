bc5abfcd1e91a03aee57f1841d05ebc0e707ffdd
Makinilla-maker <valverdeoriol@gmail.com>
2022-05-14 19:14:06 +0200

    What-er implementes pero there's a bug

--- UnityAudio/Assets/Scripts/RiverScript.cs
+++ UnityAudio/Assets/Scripts/RiverScript.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class RiverScript : MonoBehaviour
 {
-    public AudioSource riverAudioSource;
+    //public AudioSource riverAudioSource;
     private bool doOnetime;
+    public Transform[] waypoints;
+    private Vector3[] waypointsVector;
+    public Transform player;
+    private Transform trans;
     // Start is called before the first frame update
     void Start()
     {
+        waypointsVector = new Vector3[waypoints.Length];
         doOnetime = false;
+        for(int i = 0; i<waypoints.Length;i++)
+        {
+            waypointsVector[i] = waypoints[i].transform.position;
+        }
+        trans = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        System.Array.Sort<Vector3> (waypointsVector, delegate(Vector3 way1, Vector3 way2) {
+            return Vector3.Distance(way1, player.position).CompareTo(Vector3.Distance(way2, player.position));
+        });
+        
+        trans.position = Vector3.Lerp(trans.position, ClosestPointOnLine(waypointsVector[0], waypointsVector[1], player.position), Time.deltaTime * 2);
     }
-    private void OnTriggerEnter(Collider other)
+    Vector3 ClosestPointOnLine(Vector3 vA, Vector3 vB, Vector3 vPoint)
     {
-        if(!doOnetime)
-        {
-            riverAudioSource.Play();
-            doOnetime = true;
-        }
-        if(other.tag == "RiverTrigger")
-        {
-            Debug.Log(other.name + "activated");
-            riverAudioSource.transform.position = other.transform.position;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-
+        var vVector1 = vPoint - vA;
+        var vVector2 = (vB - vA).normalized;
+ 
+        var d = Vector3.Distance(vA, vB);
+        var t = Vector3.Dot(vVector2, vVector1);
+ 
+        if (t <= 0)
+            return vA;
+ 
+        if (t >= d)
+            return vB;
+ 
+        var vVector3 = vVector2 * t;
+ 
+        var vClosestPoint = vA + vVector3;
+ 
+        return vClosestPoint;
     }
 }
